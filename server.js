864ba5acb57618d01995f644f0aa3d0ab4fe1249
Hector Lopez <hectorlopezp02@gmail.com>
2021-05-13 13:08:36 -0500

    se instala body parser

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express=require('express'); //paquet para servidores
+const router=express.Router()// separa peticiones
+
+
 var app=express();//inicializa express
 
-//me va responder desde cualquier ruta
-//una func http tiene req y res
-app.use('/',function(req,res){
-    res.send('hola');
+// //me va responder desde cualquier ruta
+// //una func http tiene req y res
+// app.use('/',function(req,res){
+//     res.send('hola');
+// });
+
+app.use(router);
+router.get('/message',function(req,res){
+    res.send('lista de mensajes');
 });
+router.post('/message',function(req,res){
+    res.send('mensaje creado');
+});
+
 //ejecutar express
 app.listen(3000);
 console.log('La app esta escuchando en http://localhost:3000');
