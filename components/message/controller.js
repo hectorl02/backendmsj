64e24a43be877897752eda94e5087b805b82c185
Hector Lopez <hectorlopezp02@gmail.com>
2021-05-15 17:29:11 -0500

    se crea y configura capa de controlador para mensaje

---
+++ components/message/controller.js
@@ -0,0 +1,29 @@
+//crae funciones necesarias
+
+
+function addMessage(user,message){
+    //se trabaja con promesas para verificacion
+    return new Promise((resolve, reject)=>{
+        if(!user || !message){// si no hay usuario o mensaje,haga:
+            console.error('[messageController] No hay Usuario o mensaje');//mostrar donde esta el error
+            reject('Los datois son incorrectos');
+            return false;//para que no siga ejecutando
+        }
+        
+        const fullMessage={
+            user:user,
+            message:message,
+            date:new Date,
+        };    
+        console.log(fullMessage);
+        resolve(fullMessage);
+    });
+
+    
+
+}
+
+//exportar funcion con unico modulo
+module.exports={
+    addMessage,
+}
--- components/message/network.js
+++ components/message/network.js
@@ -1,15 +1,23 @@
 // recibe peticion http, procesar la informacion y enviar al controlador
 const express=require('express');
+const response= require('../../network/response');//traer modulo local
+const controller = require('./controller');
 const router=express.Router();
-const response= require('../../network/respond');//traer modulo local
+
 
 router.get('/',function(req,res){
     response.success(req,res,'lista de mensajes');// va al modulo response,accede a success y lleva req y res
 });
 router.post('/',function(req,res){
-    console.log(req.body);
-    console.log(req.query);
-    response.success(req,res,'mensaje creado');
+
+    controller.addMessage(req.body.user, req.body.message)
+        .then((fullMessage)=>{
+            response.success(req,res,fullMessage, 200);
+        })
+        .catch(e=>{
+            response.error(req,res,'Info invalida',400, 'Error en el controller')
+        });
+
 });
 
 module.exports=router;
